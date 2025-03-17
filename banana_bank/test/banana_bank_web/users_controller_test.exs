81881a508a0c6d65040279758d05a73e6363e54a
GaabrielFerreira <GaabrielFerreira>
2025-03-17 14:50:07 -0300

    testing controller pt3

--- banana_bank/test/banana_bank_web/users_controller_test.exs
+++ banana_bank/test/banana_bank_web/users_controller_test.exs
@@ -1,6 +1,9 @@
 defmodule BananaBankWeb.UsersControllerTest do
   use BananaBankWeb.ConnCase
 
+  alias BananaBank.Users
+  alias Users.User
+
   describe "create/2" do
     test "successfully creates an user", %{conn: conn} do
       params = %{
@@ -35,6 +38,31 @@ defmodule BananaBankWeb.UsersControllerTest do
         |> json_response(:bad_request)
 
       expected_response = %{"error" => %{"cep" => ["should be 8 character(s)"], "name" => ["can't be blank"], "password" => ["should be at least 6 character(s)"]}}
+
+      assert response == expected_response
+    end
+  end
+
+  describe "delete/2" do
+    test "successfully deletes an user", %{conn: conn} do
+      params = %{
+        name: "Joao",
+        cep: "12345678",
+        email: "joao@frutas.com",
+        password: "123456"
+      }
+
+      {:ok, %User{id: id}} = Users.create(params)
+
+      response =
+        conn
+        |> delete(~p"/api/users/#{id}")
+        |> json_response(:ok)
+
+      expected_response = %{
+        "data" => %{"cep" => "12345678", "email" => "joao@frutas.com", "id" => id, "name" => "Joao"},
+        "message" => "User deleted with successful"
+      }
 
       assert response == expected_response
     end
