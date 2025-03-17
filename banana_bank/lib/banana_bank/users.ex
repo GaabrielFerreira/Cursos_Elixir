841fba9bf58f189f5c45c7ca8c11c32d7392ba34
GaabrielFerreira <GaabrielFerreira>
2025-03-17 08:58:40 -0300

    action show creation

--- banana_bank/lib/banana_bank/users.ex
+++ banana_bank/lib/banana_bank/users.ex
@@ -1,4 +1,8 @@
 defmodule BananaBank.Users do
+
   alias BananaBank.Users.Create
+  alias BananaBank.Users.Get #alias separado para facilitar busca
+
   defdelegate create(params), to: Create, as: :call #Users.create chama o Modulo Create na funcao call
+  defdelegate get(params), to: Get, as: :call #Users.Get chama o Modulo get na funcao call
 end
--- banana_bank/lib/banana_bank_web/controllers/users_controller.ex
+++ banana_bank/lib/banana_bank_web/controllers/users_controller.ex
@@ -9,8 +9,16 @@ defmodule BananaBankWeb.UsersController do
   def create(conn, params) do
     with ({:ok, %User{} = user} <- Users.create(params)) do
       conn
-      |> put_status(:created) #created = status 200
+      |> put_status(:created) #created = status 201
       |> render(:create, user: user)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    with ({:ok, %User{} = user} <- Users.get(id)) do
+      conn
+      |> put_status(:ok) #ok = status 200
+      |> render(:get, user: user)
     end
   end
 end
