07aa7ad4ad1793a2659219d89db892d83827d0ec
GaabrielFerreira <ga.ferreiraqueiroz@hotmail.com>
2025-03-26 11:22:55 -0300

    feat: verifying users password

---
+++ banana_bank/lib/banana_bank/users/verify.ex
@@ -0,0 +1,18 @@
+defmodule BananaBank.Users.Verify do
+  alias BananaBank.Users
+  alias Users.User
+
+  def call(%{"id" => id, "password" => password}) do
+    case Users.get(id) do
+      {:ok, user} -> verify(user, password)
+      {:error, _} = error -> error #Se id for invalido
+    end
+  end
+
+  defp verify(user, password) do
+    case  Argon2.verify_pass(password, user.password_hash) do
+      true -> {:ok, :valid_password} #Se a senha for valida
+      false -> {:error, :unauthorized} #Se a senha for invalida
+    end
+  end
+end
