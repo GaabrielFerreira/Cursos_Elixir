10b1c4925ca8ce832f6baac74a3a352300d10332
GaabrielFerreira <ga.ferreiraqueiroz@hotmail.com>
2025-03-28 12:13:17 -0300

    config: add absinthe dep (to use graphql) and config graphql router

--- twix/lib/twix.ex
+++ twix/lib/twix.ex
@@ -1,9 +1,13 @@
 defmodule Twix do
+  alias Twix.Posts
   alias Twix.Users
 
+  #Posts actions
+  defdelegate create_post(params), to: Posts.Create, as: :call
+  defdelegate add_like_to_post(id), to: Posts.AddLike, as: :call
+
+  #Users actions
   defdelegate create_user(params), to: Users.Create, as: :call
   defdelegate get_user(id), to: Users.Get, as: :call
   defdelegate update_user(params), to: Users.Update, as: :call
-
-
 end
