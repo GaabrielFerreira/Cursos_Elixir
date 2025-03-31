dc556efb4e7d6882d6b88eaffb03c1b608d7fce0
GaabrielFerreira <ga.ferreiraqueiroz@hotmail.com>
2025-03-31 11:49:02 -0300

    docs: insert documentation with desc

--- twix/lib/twix_web/schema/types/user.ex
+++ twix/lib/twix_web/schema/types/user.ex
@@ -1,10 +1,11 @@
 defmodule TwixWeb.Schema.Types.User do
   use Absinthe.Schema.Notation
 
+  @desc "User logic representation. Banana Description"
   object :user do
     field :id, non_null(:id)
     field :nickname, non_null(:string)
-    field :age, non_null(:integer)
+    field :age, non_null(:integer), description: "Needs to be greather than or equal to 18"
     field :email, non_null(:string)
     field :posts, list_of(:post)
   end
