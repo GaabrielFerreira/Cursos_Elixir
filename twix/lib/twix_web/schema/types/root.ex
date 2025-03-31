2dd2c64e6f7e2dc3f1b30ebcd569a757e73a6500
GaabrielFerreira <ga.ferreiraqueiroz@hotmail.com>
2025-03-31 09:58:05 -0300

    feat: using the crudry lib

--- twix/lib/twix_web/schema/types/root.ex
+++ twix/lib/twix_web/schema/types/root.ex
@@ -1,4 +1,6 @@
 defmodule TwixWeb.Schema.Types.Root do
+
+  alias Crudry.Middlewares.TranslateErrors
   use Absinthe.Schema.Notation
 
   alias TwixWeb.Resolvers.User, as: UserResolver
@@ -17,12 +19,14 @@ defmodule TwixWeb.Schema.Types.Root do
       arg :input, non_null(:create_user_input)
 
       resolve &UserResolver.create/2
+      middleware TranslateErrors
     end
 
     field :update_user, type: :user do
       arg :input, non_null(:update_user_input)
 
       resolve &UserResolver.update/2
+      middleware TranslateErrors
     end
   end
 end
