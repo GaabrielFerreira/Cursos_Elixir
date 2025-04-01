6e2cfc814066f7091cde85cddd371c8eb2bc47ae
GaabrielFerreira <ga.ferreiraqueiroz@hotmail.com>
2025-04-01 11:08:03 -0300

    test: subscription test

--- twix/test/twix_web/schema_test.exs
+++ twix/test/twix_web/schema_test.exs
@@ -80,4 +80,59 @@ defmodule TwixWeb.SchemaTest do
       assert %{"data" => %{"createUser" => %{"id" => _id, "nickname" => "Gaaa"}}} = response
     end
   end
+
+  describe "subscriptions" do
+    test "new follow", %{socket: socket} do
+      user_params = %{nickname: "jose1", age: 22, email: "jose1@frutas.com"}
+      {:ok, user1} = Twix.create_user(user_params)
+
+      user_params = %{nickname: "jose2", age: 22, email: "jose2@frutas.com"}
+      {:ok, user2} = Twix.create_user(user_params)
+
+      mutation = """
+        mutation{
+          addFollower(input: {userId: #{user1.id}, followerId: #{user2.id}}) {
+            followerId
+            followingId
+          }
+        }
+      """
+
+      subscription = """
+        subscription{
+          newFollow{
+            followerId
+            followingId
+          }
+        }
+      """
+
+      # Setup da subscription
+      socket_ref = push_doc(socket, subscription)
+      assert_reply(socket_ref, :ok, %{subscriptionId: subscription_id})
+
+      # Setup da mutation
+      socket_ref = push_doc(socket, mutation)
+      assert_reply(socket_ref, :ok, mutation_response)
+
+      expected_response = %{
+        data: %{"addFollower" => %{"followerId" => "#{user2.id}", "followingId" => "#{user1.id}"}}
+      }
+
+      assert mutation_response == expected_response
+
+      assert_push("subscription:data", subscription_response)
+
+      expected_sub_response = %{
+               result: %{
+                 data: %{
+                   "newFollow" => %{"followerId" => "#{user2.id}", "followingId" => "#{user1.id}"}
+                 }
+               },
+               subscriptionId: "#{subscription_id}"
+             }
+
+      assert subscription_response == expected_sub_response
+    end
+  end
 end
