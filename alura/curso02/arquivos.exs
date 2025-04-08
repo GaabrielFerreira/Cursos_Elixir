dd1f0f0ba3a5a65a02ddeac993c0f73e642e0d0f
GaabrielFerreira <ga.ferreiraqueiroz@hotmail.com>
2025-04-08 12:35:09 -0300

    feat: file read with error handling

--- alura/curso02/arquivos.exs
+++ alura/curso02/arquivos.exs
@@ -2,6 +2,11 @@ defmodule Curso02.Arquivos do
   def ler(arquivo) do
     #File.read retorna tupla com :ok e conteudo ou :error e erro
     #File.read! retorna o conteudo do arquivo ou erro ao ler arquivo
-    File.read!(arquivo)
+     case File.read(arquivo) do
+       {:ok, conteudo} -> IO.puts(conteudo)
+       {:error, :enoent} -> "Arquivo inexistente"
+       {:error, :eacces} -> "Arquivo sem permissao de leitura"
+       _ -> "Erro desconhecido"
+     end
   end
 end
