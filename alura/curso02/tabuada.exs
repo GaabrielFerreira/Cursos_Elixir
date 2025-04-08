e539bdf04fda7fb7d052f05ed2043b36a4eb8467
GaabrielFerreira <ga.ferreiraqueiroz@hotmail.com>
2025-04-08 12:24:52 -0300

    feat: making a multiplication table

---
+++ alura/curso02/tabuada.exs
@@ -0,0 +1,11 @@
+defmodule Curso02.Tabuada do
+  def calcula(multiplicador) do
+    tabuada(multiplicador, 1)
+  end
+
+  defp tabuada(_, 11), do: []
+
+  defp tabuada(produto1, produto2) do
+    [produto1 * produto2] ++ tabuada(produto1, produto2 + 1)
+  end
+end
