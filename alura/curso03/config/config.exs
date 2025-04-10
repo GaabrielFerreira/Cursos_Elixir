0d56218a9945020d948b19d2e182ce5d418ce2f7
GaabrielFerreira <ga.ferreiraqueiroz@hotmail.com>
2025-04-10 11:15:09 -0300

    feat: scheduling task

---
+++ alura/curso03/config/config.exs
@@ -0,0 +1,5 @@
+use Mix.Config
+
+config :curso03, Curso03.Agendador, jobs: [
+  {"* * * * *", fn -> IO.puts("Executando tarefa agendada") end}
+]
--- alura/curso03/test/curso03_test.exs
+++ alura/curso03/test/curso03_test.exs
@@ -5,4 +5,8 @@ defmodule Curso03Test do
   test "greets the world" do
     assert Curso03.hello() == :world
   end
+
+  test "teste que falha de proposito" do
+    assert Curso03.hello() != :world
+  end
 end
