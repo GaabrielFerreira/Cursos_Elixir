c7c52f318910936719c5a6526abb6fca97c54eb1
GaabrielFerreira <ga.ferreiraqueiroz@hotmail.com>
2025-04-10 12:19:20 -0300

    folders organization and alura course completed

--- alura/curso03/test/curso03/escreve_numero_aleatorio_test.exs
+++ alura/curso03/test/curso03/escreve_numero_aleatorio_test.exs
@@ -1,5 +1,6 @@
 defmodule Curso03.EscreveNumeroAleatorioTest do
   use ExUnit.Case
+  import Mock
 
   test "2 escritas no arquivo geram numeros diferentes" do
     Curso03.EscreveNumeroAleatorio.escreve()
@@ -17,5 +18,19 @@ defmodule Curso03.EscreveNumeroAleatorioTest do
     ]))
 
     assert primeiro_conteudo != segundo_conteudo
+  end
+
+  test "2 escritas ..., com mock" do
+    :ets.new(:conteudos, [:set, :private, :named_table])
+    with_mock File, [write!: fn (_path, conteudo) -> :ets.insert_new(:conteudos, {conteudo})end] do
+      Curso03.EscreveNumeroAleatorio.escreve()
+      Curso03.EscreveNumeroAleatorio.escreve()
+    end
+
+    conteudos = :ets.tab2list(:conteudos)
+    [primeiro_valor | conteudos] = conteudos
+    [segundo_valor | _] = conteudos
+
+    assert primeiro_valor != segundo_valor
   end
 end
